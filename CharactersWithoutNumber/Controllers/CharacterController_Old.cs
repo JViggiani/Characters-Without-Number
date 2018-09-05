1054c996f526943512b51c1ead788097bf10b645
JViggiani <josh.viggiani@blueyonder.co.uk>
2018-09-05 21:42:31 +0100

    Tidyup working - characters renamed to character and made consistant over views / controller in csproj

---
+++ CharactersWithoutNumber/Controllers/CharacterController_Old.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CharactersWithoutNumber.Models;
+
+namespace CharactersWithoutNumber.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Character Sheet Placeholder.";
+
+            return View();
+        }
+
+        public IActionResult Manual()
+        {
+            ViewData["Message"] = "Character Sheet Placeholder.";
+            var myCharacter = new Character()
+            {
+                ID = 1,
+                Name = "Josh",
+                Species = "Human",
+                Gender = "Male"
+            };
+
+            //myCharacter.Class = new List<Class>();
+
+            return View(myCharacter);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
